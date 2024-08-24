a4ef53a2b0467f95fe263ab72d5c2ed8b8f05363
ramilka <lilya1976.03.03@gmail.com>
2024-08-24 12:04:30 +0600

    Init app.

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors, {CorsOptions} from 'cors';
+
+const app = express();
+const port = 8000;
+
+const whitelist = ['http://localhost:5173'];
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('public'))
+
+const run = async () => {
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+};
+
+run().catch(console.error);
+
